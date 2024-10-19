8080b7ce9d3c1762de95c03c76cf03cb4db9d49d
Daniel figueroa <daniel.figueroamanjarres@unicolombo.edu.co>
2024-10-19 16:47:11 -0500

    Ventana principal del administrador implementada - prueba

--- src/co/edu/unicolombo/s3/poo/GestionCentroComercial/Main/Main.java
+++ src/co/edu/unicolombo/s3/poo/GestionCentroComercial/Main/Main.java
@@ -1,5 +1,7 @@
 package co.edu.unicolombo.s3.poo.GestionCentroComercial.Main;
 
+import co.edu.unicolombo.s3.poo.GestionCentroComercial.GUI.PrincipalAdminVentana;
+
 /**
  *
  * @author CLEYMER
@@ -7,5 +9,10 @@ package co.edu.unicolombo.s3.poo.GestionCentroComercial.Main;
 public class Main {
     public static void main(String []args){
         System.out.println("Testing the app");
+        
+        PrincipalAdminVentana principalAdminVentana = new PrincipalAdminVentana();
+        principalAdminVentana.setTitle("Ventana Principal de administración");
+        principalAdminVentana.setVisible(true);
+        principalAdminVentana.setLocationRelativeTo(null);
     }
 }
